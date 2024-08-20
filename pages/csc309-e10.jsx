8449031df65ecca25a4b4181efec7adbd8e112c5
Kianoosh Abbasi <kianoosh.abbasi76@gmail.com>
2024-08-19 20:56:13 -0400

    e10

--- pages/csc309-e10.jsx
+++ pages/csc309-e10.jsx
@@ -1,7 +1,7 @@
 export default function handler(){
 	return (
 		<div id="auto-tester">
-			  Hi. I'm abbasiki, and I've learned how to develop a full-stack web app and
+			  Hi. I am abbasiki, and I have learned how to develop a full-stack web app and
   deploy it!
 		</div>
 	)
