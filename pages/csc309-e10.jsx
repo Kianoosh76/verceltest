d75032fc2cece379828be39c606a586e384f9922
Kianoosh Abbasi <kianoosh.abbasi76@gmail.com>
2024-08-19 20:41:02 -0400

    added auto tester

---
+++ pages/csc309-e10.jsx
@@ -0,0 +1,8 @@
+export default function handler(){
+	return (
+		<div id="auto-tester">
+			  Hi. I'm abbasiki, and I've learned how to develop a full-stack web app and
+  deploy it!
+		</div>
+	)
+}
